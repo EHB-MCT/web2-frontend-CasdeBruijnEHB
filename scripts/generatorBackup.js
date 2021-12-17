190516a89cd50f6e459e2654d7a144a387d27604
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-12-17 21:28:47 +0100

    Update generator. Scores werken - resultpage opbouwen nu

--- scripts/generatorBackup.js
+++ scripts/generatorBackup.js
@@ -1,7 +1,8 @@
 import * as loaderAnimation from './loader.js'
 let containerImages = document.getElementById("GeneratorContainer");
 let progressBarClicks;
-let calculateScore;
+let playlistScores = [];
+let chosenPictureObject = [];
 let progressBarCircles = document.getElementsByClassName("progressItems");
 
 //This variable is used to save the urls of the chosen images. In order to prohibit duplicates
@@ -114,18 +115,45 @@ const playlistGenerator = {
         generatorImageButtons();
     },
     updateHTML(maincategory) {
-
+        console.log(maincategory)
         let randomIndexOne;
         let randomIndexTwo;
         let html;
 
-        //Hier worden de URLS van de images uit de juiste category in een array geplaatst.
-        //Ze worden er in geplaatst zodat er op een makkelijke manier duplicaten verwijderd kunnen worden.
-        if (chosenImagesUrls.length <= 1) {
-            chosenImagesUrls = [];
-            for (let i = 0; i < playlistGenerator[`playlist_${maincategory}`].length; i++) {
-                chosenImagesUrls.push(playlistGenerator[`playlist_${maincategory}`][i].imageUrl);
-            }
+        //Hier wordt de html upgedate naargelang de category van gekozen foto. 
+        //Er zijn drie categorieën met foto's.
+        //console.log(playlistScores);
+        switch (maincategory) {
+            case "brownCreamGreenBlue":
+                //Eerst kijken of er al iets in de array zit (mag maar 1x toevoegen. Daarna de juiste urls toevoegen.)
+                if (chosenImagesUrls.length <= 1) {
+                    chosenImagesUrls = [];
+                    for (let i = 0; i < playlistGenerator.playlist_brownCreamGreenBlue.length; i++) {
+                        chosenImagesUrls.push(playlistGenerator.playlist_brownCreamGreenBlue[i].imageUrl);
+                        //playlistScores.push(playlistGenerator.playlist_brownCreamGreenBlue[i].score)
+                    }
+                }
+                break;
+            case "orangeYellowPink":
+                //Eerst kijken of er al iets in de array zit (mag maar 1x toevoegen. Daarna de juiste urls toevoegen.)
+                if (chosenImagesUrls.length <= 1) {
+                    chosenImagesUrls = [];
+                    for (let i = 0; i < playlistGenerator.playlist_orangeYellowPink.length; i++) {
+                        chosenImagesUrls.push(playlistGenerator.playlist_orangeYellowPink[i].imageUrl);
+                        //playlistScores.push(playlistGenerator.playlist_orangeYellowPink[i].score)
+                    }
+                }
+                break;
+            case "purpleBlackRed":
+                if (chosenImagesUrls.length <= 1) {
+                    chosenImagesUrls = [];
+                    for (let i = 0; i < playlistGenerator.playlist_purpleBlackRed.length; i++) {
+                        chosenImagesUrls.push(playlistGenerator.playlist_purpleBlackRed[i].imageUrl);
+                        //playlistScores.push(playlistGenerator.playlist_purpleBlackRed[i].score)
+
+                    }
+                }
+                break;
         }
 
 
@@ -141,6 +169,24 @@ const playlistGenerator = {
         <img class="generatorImages" id="${maincategory}" src="${chosenImagesUrls[randomIndexOne]}" alt="Image one">
         <img class="generatorImages" id="${maincategory}" src="${chosenImagesUrls[randomIndexTwo]}" alt="Image Two">`;
 
+
+        //Hier worden ze in een aparte array gestopt om de score te kunnen ophalen
+        switch (maincategory) {
+            case "brownCreamGreenBlue":
+                chosenPictureObject.push(playlistGenerator.playlist_brownCreamGreenBlue[randomIndexOne]);
+                chosenPictureObject.push(playlistGenerator.playlist_brownCreamGreenBlue[randomIndexTwo]);
+                break;
+            case "orangeYellowPink":
+                chosenPictureObject.push(playlistGenerator.playlist_orangeYellowPink[randomIndexOne]);
+                chosenPictureObject.push(playlistGenerator.playlist_orangeYellowPink[randomIndexTwo]);
+                break;
+            case "purpleBlackRed":
+                chosenPictureObject.push(playlistGenerator.playlist_purpleBlackRed[randomIndexOne]);
+                chosenPictureObject.push(playlistGenerator.playlist_purpleBlackRed[randomIndexTwo]);
+                break;
+        }
+
+
         //Nu moeten de foto urls uit de array verwijderd worden.
         //eerste kan gewoon met splice maar daarna worden de indexes veranderd dus moet er een -1 of +1 bijkomen.
         chosenImagesUrls.splice(randomIndexOne, 1)
@@ -149,11 +195,16 @@ const playlistGenerator = {
         } else {
             chosenImagesUrls.splice(randomIndexTwo + 1, 1)
         }
+
+        console.log("chosen objects ", chosenPictureObject)
+
         //Loading animatie 
         loaderAnimation.generatorAnimation();
         //Toevoegen HTML en functies toewijzen aan de images
         containerImages.innerHTML = html;
         generatorImageButtons(randomIndexOne, randomIndexTwo);
+
+
     }
 }
 
@@ -161,8 +212,8 @@ init();
 
 function init() {
     //Deze variabelen wordt gebruikt voor de eerstvolgende progressbar circle te kiezen & opvullen.
-    calculateScore = 0;
     progressBarClicks = 1;
+    console.log("Aantal categories", playlistGenerator.category_images);
     playlistGenerator.initiateGenerator();
 }
 
@@ -176,34 +227,21 @@ function generatorImageButtons() {
             if (progressBarClicks < 3) {
                 progressBarCircles[progressBarClicks].classList.add("active");
                 progressBarClicks++;
+                //console.log("Click button", playlistGenerator.category_images[i].mainCategory)
+                //Bij de klik moet je de category meegeven van de desbetreffende foto, zodat de volgende ronde er op wordt afgestemd
+                console.log(e.target.id)
+                console.log("Tryout", playlistGenerator[`playlist_${e.target.id}`]);
 
-                //Functie aangeroepen die de playlist score berekend.
-                generateScore(e.target);
-                //calculateScore+= playlistGenerator[`playlist_${e.target.id}`][randomIndexOne].score
                 playlistGenerator.updateHTML(e.target.id);
-            } else if (progressBarClicks >= 3) {
-                //Wanneer de progressbar vol is = de quiz afgelopen en mag het resultaat weergeven worden.
-                generateScore(e.target);
+            } else if (progressBarCircles > 3) {
                 fillResultPage();
             }
+            //Na klikken worden de gekozen objecten weer leeg gemaakt omdat er een nieuwe ronde is.
+            chosenPictureObject = [];
         })
     }
 }
 
-function generateScore(elementID) {
-    playlistGenerator[`playlist_${elementID.id}`].forEach(element => {
-        let urlTarget = elementID.src;
-        urlTarget = urlTarget.substring(urlTarget.indexOf("Images") - 1);
-        let newUrl = "." + urlTarget;
-        if (newUrl === element.imageUrl) {
-            console.log("YEP", element.imageUrl)
-            console.log("YEP", element.score)
-            calculateScore += parseInt(element.score);
-        }
-    });
-}
-
 function fillResultPage(score, category) {
-    console.log("Fill result");
-    console.log("totaal:", calculateScore)
+
 }
