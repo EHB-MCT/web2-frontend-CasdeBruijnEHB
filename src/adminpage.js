0e3e88f33773b3333003fc151730ae095ad4571d
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2022-01-30 22:36:07 +0100

    Page disabled

--- src/adminpage.js
+++ src/adminpage.js
@@ -1,10 +1,10 @@
 let container = document.getElementById("containerAdmin");
-let buttonAddCuratedPlaylist = document.getElementById("buttonAddCuratedPlaylist");
-let buttonAddGeneratedPlaylist = document.getElementById("buttonAddGeneratedPlaylist");
-let buttonRemovePlaylist = document.getElementById("buttonDeletePlaylist");
+//let buttonAddCuratedPlaylist = document.getElementById("buttonAddCuratedPlaylist");
+//let buttonAddGeneratedPlaylist = document.getElementById("buttonAddGeneratedPlaylist");
+//let buttonRemovePlaylist = document.getElementById("buttonDeletePlaylist");
 
 
-
+/*
 buttonAddCuratedPlaylist.addEventListener("click", function () {
     container.innerHTML = "";
     let html = `<form>
@@ -30,7 +30,7 @@ buttonAddCuratedPlaylist.addEventListener("click", function () {
             let description = document.getElementById("description");
             let imageurl = document.getElementById("imageurl");
             addCuratedPlaylist(playlistID, title, description, imageurl);
-            
+
             addPlaylistButton.innerHTML = "Added!"
         }
 
@@ -58,9 +58,9 @@ buttonAddGeneratedPlaylist.addEventListener("click", function () {
     container.innerHTML = html;
     let addPlaylistButton = document.getElementById("formbuttonAddPlaylist");
     addPlaylistButton.addEventListener("click", function (e) {
-        
+
         e.preventDefault();
-        
+
 
         if (addPlaylistButton.innerHTML != "Added!") {
             let playlistID = document.getElementById("playlistID");
@@ -70,11 +70,12 @@ buttonAddGeneratedPlaylist.addEventListener("click", function () {
             let mainCategory = document.getElementById("mainCategory");
             let score = document.getElementById("score");
             addGeneratedPlaylist(playlistID, title, description, imageurl, mainCategory, score);
-            
+
             addPlaylistButton.innerHTML = "Added!"
         }
     })
 })
+*/
 /*
 buttonRemovePlaylist.addEventListener("click", function () {
     container.innerHTML = "";
@@ -110,7 +111,7 @@ async function addCuratedPlaylist(playlistID, title, description, imageurl) {
     }).then(response => {
         return response.json()
     }).then(data => {
-   
+
     })
 }
 
@@ -132,6 +133,6 @@ async function addGeneratedPlaylist(playlistID, title, description, imageurl, ma
     }).then(response => {
         return response.json()
     }).then(data => {
-        
+
     })
 }
